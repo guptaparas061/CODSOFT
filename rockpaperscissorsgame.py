4e300a20e6a990f523075379829808c23c67d93d
Paras Gupta <guptaparas061@gmail.com>
2023-10-18 07:19:52 +0530

    Add files via upload

--- rockpaperscissorsgame.py
+++ rockpaperscissorsgame.py
@@ -1,58 +1,71 @@
+import tkinter as tk
 import random
 
-def get_user_choice():
-    while True:
-        user_choice = input("Choose rock, paper, or scissors: ").lower()
-        if user_choice in ['rock', 'paper', 'scissors']:
-            return user_choice
-        else:
-            print("Invalid choice. Please choose rock, paper, or scissors.")
 
-def get_computer_choice():
-    return random.choice(['rock', 'paper', 'scissors'])
+def play_round(user_choice):
+    computer_choice = random.choice(choices)
+    winner = determine_winner(user_choice, computer_choice)
+
+    result_label.grid(row=3, columnspan=3, pady=10)
+
+    if winner == "Tie":
+        result_var.set("It's a tie!")
+    else:
+        result_var.set(f"{winner} wins!")
+
+    if winner == "User":
+        user_score_var.set(user_score_var.get() + 1)
+    elif winner == "Computer":
+        computer_score_var.set(computer_score_var.get() + 1)
+
 
 def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         return "Tie"
-    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
-         (user_choice == 'scissors' and computer_choice == 'paper') or \
-         (user_choice == 'paper' and computer_choice == 'rock'):
+    elif (
+        (user_choice == "Rock" and computer_choice == "Scissors")
+        or (user_choice == "Scissors" and computer_choice == "Paper")
+        or (user_choice == "Paper" and computer_choice == "Rock")
+    ):
         return "User"
     else:
         return "Computer"
 
-def play_round():
-    user_choice = get_user_choice()
-    computer_choice = get_computer_choice()
-    winner = determine_winner(user_choice, computer_choice)
-    
-    print(f"\nUser choice: {user_choice}")
-    print(f"Computer choice: {computer_choice}")
-    
-    if winner == "Tie":
-        print("It's a tie!")
-    else:
-        print(f"{winner} wins!")
 
-    return winner
+def play_again():
+    result_var.set("")
+    result_label.grid_forget()
+    user_score_var.set(0)
+    computer_score_var.set(0)
 
-def play_game():
-    user_score = 0
-    computer_score = 0
 
-    while True:
-        winner = play_round()
+choices = ["Rock", "Paper", "Scissors"]
 
-        if winner == "User":
-            user_score += 1
-        elif winner == "Computer":
-            computer_score += 1
+root = tk.Tk()
+root.title("Rock-Paper-Scissors Game")
 
-        print(f"\nUser score: {user_score}")
-        print(f"Computer score: {computer_score}")
+result_var = tk.StringVar()
+user_score_var = tk.IntVar()
+computer_score_var = tk.IntVar()
 
-        play_again = input("\nDo you want to play again? (yes/no): ").lower()
-        if play_again != 'yes':
-            break
+frame = tk.Frame(root)
+frame.pack(padx=10, pady=10)
 
-play_game()
+tk.Label(frame, text="Rock Paper Scissors", font="normal 20 bold", fg="green").grid(row=0, column=0, columnspan=3, pady=5)
+
+user_label = tk.Label(frame, text="User", font='10').grid(row=1, column=0, padx=10, pady=10)
+vs_label = tk.Label(frame, text="VS", font="normal 10 bold").grid(row=1, column=1, padx=10, pady=10)
+computer_label = tk.Label(frame, text="Computer", font='10').grid(row=1, column=2, padx=10, pady=10)
+
+result_label = tk.Label(frame, textvariable=result_var, font="normal 20 bold", bg="white", width=15, borderwidth=2, relief="solid")
+
+tk.Button(frame, text="Rock", font='10', width=7, command=lambda: play_round("Rock")).grid(row=5, column=0, padx=10, pady=10)
+tk.Button(frame, text="Paper", font='10', width=7, command=lambda: play_round("Paper")).grid(row=5, column=1, padx=10, pady=10)
+tk.Button(frame, text="Scissors", font='10', width=7, command=lambda: play_round("Scissors")).grid(row=5, column=2, padx=10, pady=10)
+
+tk.Button(frame, text="Play Again", font='10',fg="white", bg="black", command=play_again).grid(row=6, columnspan=3, pady=10)
+
+tk.Label(frame, textvariable=user_score_var, font='10').grid(row=2, column=0, padx=10, pady=5)
+tk.Label(frame, textvariable=computer_score_var, font='10').grid(row=2, column=2, padx=10, pady=5)
+
+root.mainloop()
